aa4de473ef8e5482af3ce6423884b49a70b1f2f4
WilliamCorotan <williamcorotan@gmail.com>
2023-05-17 09:38:29 +0800

    added templates

---
+++ application/models/Model_template.php
@@ -0,0 +1,56 @@
+<?php
+
+class Name_Model extends CI_Model {
+
+    /**
+     * 
+     * Fetches all data from the database
+     * 
+     */
+    public function get_all(){
+
+    }
+
+    /**
+     * 
+     * Fetch specific data from the database
+     * @param int $id
+     * 
+     */
+    public function get($id){
+
+    }
+
+    /**
+     * 
+     * Inserts new data into database
+     * @param array $data
+     * 
+     */
+    public function insert($data){
+
+    }
+
+    /**
+     * 
+     * Updates specific data in the database 
+     * @param int $id
+     * @param array $data
+     * 
+     */
+    public function update($id, $data){
+
+    }
+
+    /**
+     * 
+     * Deletes specific data in the database
+     * @param int $id
+     * 
+     */
+    public function delete($id){
+
+    }
+
+    
+}
