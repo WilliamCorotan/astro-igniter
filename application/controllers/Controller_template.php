311c909088b1bd26e68b69779c730757f4f43717
WilliamCorotan <williamcorotan@gmail.com>
2023-05-17 09:37:46 +0800

    added templates

---
+++ application/controllers/Controller_template.php
@@ -0,0 +1,72 @@
+<?php
+
+class Name_Controller extends CI_Controller
+{
+
+    /**
+     * 
+     * Shows all data
+     * 
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * 
+     * Shows form for creating a new data
+     * 
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * 
+     * Stores data into database
+     * 
+     */
+    public function store()
+    {
+    }
+
+    /**
+     * 
+     * Shows a specific data
+     * @param int $id
+     * 
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * 
+     * Shows form for updating a specific data
+     * @param int $id
+     * 
+     */
+    public function edit($id)
+    {
+    }
+
+    /**
+     * 
+     * Updates data and stores to database
+     * @param int $id
+     * 
+     */
+    public function update($id)
+    {
+    }
+
+    /**
+     * 
+     * Deletes specific data from the database
+     * @param int $id
+     * 
+     */
+    public function destroy($id)
+    {
+    }
+}
